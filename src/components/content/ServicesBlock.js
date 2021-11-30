4525998655905099289abfa16bcd08c972af7243
worlddeleteRin <worlddelete0@gmail.com>
2021-12-01 00:35:08 +0300

    :sparkles:

--- src/components/content/ServicesBlock.js
+++ src/components/content/ServicesBlock.js
@@ -1,7 +1,20 @@
+import {
+    Button
+} from '@mui/material';
+
 import Title from '@/components/content/Title';
 import { defaultServices } from '@/helpers/content';
 
+import { useSite } from '@/hooks/useSite';
+
+
 function ServicesBlock (props) {
+    const site = useSite()
+
+    const handleOpenCallModal = () => {
+        site.setRequestCallModal(true)
+    }
+
     const services = props.services ? props.services : defaultServices
     const title = (
         <Title
@@ -11,25 +24,54 @@ function ServicesBlock (props) {
 
     const Service = ({service}) => {
         return (
-            <div className="flex flex-col max-w-[200px]">
+            <div className="max-w-[300px]">
                 <img
                     src={service?.imgsrc}
                 />
-                <div className="">
+                <div className="font-semibold mt-2">
+                    { service?.title }
                 </div>
+                <div className="mt-1">
+                    { service?.price }
+                </div>
+                {
+
+                }
+                <Button
+                    color="primary"
+                    variant="outlined"
+                    className="w-full mt-3"
+                >
+                    Подробнее
+                </Button>
+                {
+                <Button
+                    color="primary"
+                    variant="contained"
+                    className="w-full mt-3"
+                    onClick={handleOpenCallModal}
+                >
+                    Заказать
+                </Button>
+                }
             </div>
         )
     }
 
     return (
-        <div>
-            <div className="flex flex-wrap gap-4">
-                {services?.map((service) =>
-                    <Service
-                        key={service?.id}
-                        service={service}
-                    />
-                )}
+        <div class="py-10 bg-gray-50 mt-6">
+            <div className="max-w-screen-lg mx-2 md:mx-auto">
+
+                { title }
+
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 py-8">
+                    {services?.map((service) =>
+                        <Service
+                            key={service?.id}
+                            service={service}
+                        />
+                    )}
+                </div>
             </div>
         </div>
     )
