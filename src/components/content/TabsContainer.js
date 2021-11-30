15114f9e979ca5ba0385c8242f4300788f9e8adc
worlddeleteRin <worlddelete0@gmail.com>
2021-12-01 01:22:41 +0300

    :sparkles:

--- src/components/content/TabsContainer.js
+++ src/components/content/TabsContainer.js
@@ -1,16 +1,51 @@
 import { defaultTabsContainer } from "@/helpers/content";
 
 import {
+    Tab,
 } from '@mui/material';
+import {
+    TabContext,
+    TabList,
+    TabPanel
+} from '@mui/lab';
 
 
 function TabsContainer (props) {
     const imgsrc = props?.imgsrc ? props.imgsrc : defaultTabsContainer.imgsrc
     const tabs = props?.tabs ? props.tabs : defaultTabsContainer.tabs
 
+    const [tabValue, setTabValue] = useState(1);
+
+    const handleSetTab = (event, newValue) => {
+        setValue(newValue)
+    }
+
     const tabsBlock = (
         <div>
-            tabs
+            <TabContext
+                value={tabValue}
+            >
+                <TabList
+                    onChange={handleSetTab}
+                >
+                    {tabs?.map((tab) =>
+                            <Tab
+                                label={tab?.title}
+                                value={tab?.id}
+                            >
+                            </Tab>
+                    )}
+                </TabList>
+                <div>
+                    {tabs?.map((tab) => 
+                            <> 
+                            <TabPanel value={tab?.id}>
+                                {tab?.content}
+                            </TabPanel>
+                            </>
+                    )}
+                </div>
+              </TabContext>
         </div>
     )
     const imageBlock = (
