95fc49a12299b844cc35cd41b80cb5a0d2bfcc34
worlddeleteRin <worlddelete0@gmail.com>
2021-12-06 03:43:47 +0300

    :sparkles:

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
-const api_url = "http://0.0.0.0:8000/"
-// const api_url = "http://api.drsu.fast-code.ru/"
+// const api_url = "http://0.0.0.0:8000/"
+const api_url = "http://api.drsu.fast-code.ru/"
 
 export { api_url }
